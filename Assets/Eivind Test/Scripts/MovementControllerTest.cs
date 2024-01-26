095dfa5ebe655edda8e6bed6ee15291234f13792
OneGuy666 <eflefyl@gmail.com>
2024-01-26 23:44:16 +0100

    Test Scene

---
+++ Assets/Eivind Test/Scripts/MovementControllerTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class MovementControllerTest : MonoBehaviour
+{
+    private Vector2 move;
+    public int speed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+}
