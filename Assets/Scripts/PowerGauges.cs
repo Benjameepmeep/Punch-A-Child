4e7f223881be27d6f89d63aa2fb1e73613c4586c
Benjameepmeep <benjamin.arctander@gmail.com>
2024-01-27 15:11:10 +0100

    PowerGauges Script set-up

---
+++ Assets/Scripts/PowerGauges.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerGauges : MonoBehaviour
+{
+    // TODO: Make one for power, and one for angle launched
+    // TODO: Each gauge outputs/returns their value to a function which will calculate velocity (force and direction).
+    // TODO: This function will send its data to KidTrajectoryScript, which will launch the kid.
+    
+    // TODO: Each gauge needs to receive input. Spacebar / Mouse Click.
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
