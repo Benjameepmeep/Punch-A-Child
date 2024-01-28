08ada50511718bf4b6e343cc861cf84330cf08ef
OneGuy666 <eflefyl@gmail.com>
2024-01-28 14:45:42 +0100

    scene

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void Loadlevel(string level)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    
+    /*public void LoadSceneByName(string sceneName)
+    {
+        //Start a coroutine to wait until the fade is done
+        StartCoroutine(LoadLevel(UnityEngine.SceneManagement.SceneManager.GetSceneByName(sceneName).buildIndex+1));
+        return;
+    }*/
+    
+    /*private IEnumerator LoadLevel(int levelIndex)
+    {
+        yield return new WaitForSeconds(0.01f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelIndex);
+    }*/
+}
